610c6786a8f6c00c73bb1e554cfb6a5a7ea0fc05
Francesco Zimbolo <francesco@haloghen.it>
2022-11-15 14:13:38 +0100

    Final scripts

--- robot/src/main.cpp
+++ robot/src/main.cpp
@@ -2,7 +2,6 @@
 #include <esp_wifi.h>
 #include <WiFi.h>
 
-
 #define PWM1_ch 0
 #define PWM2_ch 1
 #define PWM3_ch 2
@@ -22,16 +21,15 @@
 
 #define weapPot 10
 
-//MAC robot grab    C8:C9:A3:CB:70:54
+// MAC robot grab    C8:C9:A3:CB:70:54
 uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-typedef struct {
+typedef struct
+{
   int16_t speedmotorLeft;
   int16_t speedmotorRight;
   int8_t Fire;
-}
-packet_t;
-
+} packet_t;
 
 packet_t recData;
 
@@ -62,100 +60,126 @@ String success;
 esp_now_peer_info_t peerInfo;
 
 // Callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  //Serial.print("\r\nLast Packet Send Status:\t");
-  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  if (status == 0) {
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  // Serial.print("\r\nLast Packet Send Status:\t");
+  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status == 0)
+  {
     success = "Delivery Success :)";
   }
-  else {
+  else
+  {
     success = "Delivery Fail :(";
   }
 }
 
 // Callback when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
   memcpy(&recData, incomingData, sizeof(recData));
-  //Serial.print("Bytes received: ");
-  //Serial.println(len);
-  //salva dati in var globali
+  // Serial.print("Bytes received: ");
+  // Serial.println(len);
+  // salva dati in var globali
   recLpwm = recData.speedmotorLeft;
   recRpwm = recData.speedmotorRight;
   recFire = recData.Fire;
   lastPacketMillis = millis();
 }
 
-int setM1speed(int rpm) {
-  if (rpm < 0) {
+int setM1speed(int rpm)
+{
+  if (rpm < 0)
+  {
     ledcWrite(PWM1_ch, 0);
     ledcWrite(PWM2_ch, -rpm);
-  } else if (rpm > 0) {
+  }
+  else if (rpm > 0)
+  {
     ledcWrite(PWM1_ch, rpm);
     ledcWrite(PWM2_ch, 0);
-  } else if (rpm == 0) {
+  }
+  else if (rpm == 0)
+  {
     ledcWrite(PWM1_ch, 255);
     ledcWrite(PWM2_ch, 255);
   }
   return 0;
 }
 
-int setM2speed(int rpm) {
-  if (rpm < 0) {
+int setM2speed(int rpm)
+{
+  if (rpm < 0)
+  {
     ledcWrite(PWM3_ch, 0);
     ledcWrite(PWM4_ch, -rpm);
-  } else if (rpm > 0) {
+  }
+  else if (rpm > 0)
+  {
     ledcWrite(PWM3_ch, rpm);
     ledcWrite(PWM4_ch, 0);
-  } else if (rpm == 0) {
+  }
+  else if (rpm == 0)
+  {
     ledcWrite(PWM3_ch, 255);
     ledcWrite(PWM4_ch, 255);
   }
   return 0;
 }
 
-int setM3speed(int rpm) {
-  if (rpm < 0) {
+int setM3speed(int rpm)
+{
+  if (rpm < 0)
+  {
     ledcWrite(PWM5_ch, 0);
     ledcWrite(PWM6_ch, -rpm);
-  } else if (rpm > 0) {
+  }
+  else if (rpm > 0)
+  {
     ledcWrite(PWM5_ch, rpm);
     ledcWrite(PWM6_ch, 0);
-  } else if (rpm == 0) {
+  }
+  else if (rpm == 0)
+  {
     ledcWrite(PWM5_ch, 255);
     ledcWrite(PWM6_ch, 255);
   }
   return 0;
 }
 
-int seek_angle_smooth(int target_pos,int accel) {
-  if (target_pos < 0 || target_pos > 1023) {
+int seek_angle_smooth(int target_pos, int accel)
+{
+  if (target_pos < 0 || target_pos > 1023)
+  {
     setM3speed(0);
     return 1;
   }
   int wpn_current_pos = analogRead(weapPot);
-//  Serial.println("Pot: " + (String) wpn_current_pos);
+  Serial.println("Pot: " + (String)wpn_current_pos);
   unsigned long wpn_current_time = millis();
   float pos_delta = wpn_current_pos - wpn_prev_pos;
   unsigned long time_delta = wpn_current_time - wpn_prev_time;
-  float wpn_speed = pos_delta/time_delta;
+  float wpn_speed = pos_delta / time_delta;
   int gap = target_pos - wpn_current_pos;
   int wpn_pwm = 0;
-  int accel_treshold = abs(wpn_prev_pwm)+accel;
-  
-  if (abs(gap) > 20) {
-    wpn_pwm = target_pos -(wpn_current_pos+ wpn_speed*13);
+  int accel_treshold = abs(wpn_prev_pwm) + accel;
+
+  if (abs(gap) > 20)
+  {
+    wpn_pwm = target_pos - (wpn_current_pos + wpn_speed * 13);
   }
-  
-  if(accel> 0) wpn_pwm = constrain(wpn_pwm, -accel_treshold, accel_treshold);
+
+  if (accel > 0)
+    wpn_pwm = constrain(wpn_pwm, -accel_treshold, accel_treshold);
   wpn_pwm = constrain(wpn_pwm, -recWpnStr, recWpnStr);
   setM3speed(wpn_pwm);
 
-  //prev sets for next iteration
+  // prev sets for next iteration
   wpn_prev_pos = wpn_current_pos;
   wpn_prev_time = wpn_current_time;
   wpn_prev_speed = wpn_speed;
   wpn_prev_pwm = wpn_pwm;
-  
+
   /*
   //if(wpn_speed < 350) return 1;
   Serial.print(target_pos);
@@ -171,9 +195,10 @@ int seek_angle_smooth(int target_pos,int accel) {
   return 0;
 }
 
-void setup() {
-  //Serial.begin(115200);
-//  Serial.println("Ready.");
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Ready.");
   analogReadResolution(10);
   ledcAttachPin(IN1_gpio, PWM1_ch);
   ledcAttachPin(IN2_gpio, PWM2_ch);
@@ -194,25 +219,27 @@ void setup() {
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
   esp_wifi_set_channel(10, WIFI_SECOND_CHAN_NONE);
-  
+
   // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    //Serial.println("Error initializing ESP-NOW");
+  if (esp_now_init() != ESP_OK)
+  {
+    // Serial.println("Error initializing ESP-NOW");
     return;
   }
 
   // Once ESPNow is successfully Init, we will register for Send CB to
   // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
-  
+
   // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 10;  
+  peerInfo.channel = 10;
   peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    //Serial.println("Failed to add peer");
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    // Serial.println("Failed to add peer");
     return;
   }
   // Register for a callback function that will be called when data is received
@@ -220,24 +247,31 @@ void setup() {
   recWpnStr = 255;
 }
 
-void loop() {
+void loop()
+{
   failsafe = false;
   unsigned long current_time = millis();
-  if(current_time - lastPacketMillis > failsafeMaxMillis){
+  if (current_time - lastPacketMillis > failsafeMaxMillis)
+  {
     failsafe = true;
-    //Serial.println(current_time - lastPacketMillis);
+    // Serial.println(current_time - lastPacketMillis);
   }
-  if(failsafe){
+  if (failsafe)
+  {
     setM2speed(0);
     setM1speed(0);
     setM3speed(0);
-  }else{
+  }
+  else
+  {
     setM2speed(-recRpwm);
     setM1speed(-recLpwm);
-    if (recFire) {
+    if (recFire)
+    {
       seek_angle_smooth(wpn_end, wpn_accel);
     }
-    else {
+    else
+    {
       seek_angle_smooth(wpn_default, wpn_accel);
     }
   }
